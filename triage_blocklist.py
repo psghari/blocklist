58810de2ea8de90a6e64238857c11154764d2622
Hari Prasanth <hariprasanth.g@gmail.com>
2025-04-07 21:11:52 +0530

    Auto-update blocklist: 2025-04-07 21:11

--- triage_blocklist.py
+++ triage_blocklist.py
@@ -7,7 +7,7 @@ import re
 NEXTDNS_LOG = "nextdns_log.txt"
 ALLOWLIST_FILE = "nextdns_allowlist.txt"
 SEED_LIST_FILE = "my_blocklist_seed.txt"
-FIREBASE_DOMAINS = [,
+FIREBASE_DOMAINS = [
     "firebaselogging-pa.googleapis.com",
     "firebase-settings.crashlytics.com",
     "crashlyticsreports-pa.googleapis.com",
